591e58595b6126d0f5eca69adaf48ee36cd2fa51
pawanaditya369 <160942470+pawanaditya@users.noreply.github.com>
2024-09-30 16:05:36 -0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,7 +60,7 @@ document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
             document.title = "Pawan Aditya | Portfolio";
-            $("#favicon").attr("href", "assets/images/favicon.png");
+            $("#favicon").attr("href", "assets/images/dicon.png");
         }
         else {
             document.title = "Come Back To Portfolio";
